73b5579c50aad9177398211672b102af249ef473
Rishita <160631403+rauljirishu@users.noreply.github.com>
2026-06-27 23:34:10 +0530

    Add files via upload

---
+++ foodorder.sql
@@ -0,0 +1,27 @@
+USE data_analytics;
+
+SELECT * FROM onlinefoods;
+
+SELECT COUNT(*) AS Total_Customers
+FROM onlinefoods;
+
+SELECT DISTINCT Gender
+FROM onlinefoods;
+
+SELECT DISTINCT Occupation
+FROM onlinefoods;
+
+SELECT DISTINCT `Marital Status`
+FROM onlinefoods;
+
+SELECT DISTINCT `Monthly Income`
+FROM onlinefoods;
+
+SELECT DISTINCT `Educational Qualifications`
+FROM onlinefoods;
+
+SELECT DISTINCT Feedback
+FROM onlinefoods;
+
+SELECT DISTINCT Output
+FROM onlinefoods;
